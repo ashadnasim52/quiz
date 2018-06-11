152f8765af025da354b14d5d54cffb2505304d36
root <ashadnasim123@gmail.com>
2018-06-11 10:17:58 +0530

    intial

---
+++ app/src/main/java/appm/ashad/quiz/quiz/highscore.java
@@ -0,0 +1,25 @@
+package appm.ashad.quiz.quiz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+
+public class highscore extends AppCompatActivity {
+
+    CardView start;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highscore);
+        start=findViewById(R.id.cardView2);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),Question.class);
+                startActivity(i);
+            }
+        });
+    }
+}
